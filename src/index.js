76f3cf07ef8663622b2d24e824357a9fb563c4b4
aliharriss <aliharriss1995@gmail.com>
2021-06-20 20:55:51 +1200

    jsx done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import the react and react dom libraries to get access to them
+import React from 'react'
+import ReactDOM from 'react-dom'
+ 
+// Create a react component
+// A react component is a fn or class
+// It uses jsx to show me html
+// It handles the feedback from the user from events
+const App = () => {
+    const buttonText = {text:'click me'};
 
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name
+            </label>
+            <input id = "name" type="text" />
+            <button style = {{backgroundColor:'blue', color:'white'}}>{buttonText.text}</button>
+        </div>
+    );
+};
+
+// This enables hot reloading
+if (module.hot) {
+    module.hot.accept();
+}
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
